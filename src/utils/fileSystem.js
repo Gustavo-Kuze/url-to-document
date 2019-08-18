c8fb7277306b3cf3f4408d92e1bed000fe11cb70
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-08-17 21:36:19 -0300

    updated regex

--- src/utils/fileSystem.js
+++ src/utils/fileSystem.js
@@ -1,7 +1,10 @@
 const generateFileNameByUrl = (url, fileLocation, fileExtension = "") =>
     `${__dirname}${fileLocation}/${new Date()
         .toLocaleTimeString()
-        .replace(/[:]/, "_")}${new Date().toLocaleDateString()}${Buffer.from(
+        .replace(
+            /[:|\s]/g,
+            "_"
+        )}${new Date().toLocaleDateString()}${Buffer.from(
         url,
         "latin1"
     ).toString("base64")}${fileExtension}`;
