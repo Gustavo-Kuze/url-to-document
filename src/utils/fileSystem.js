2ea1f02ff1bd00a9e71dbaf13c185e661d151f1d
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-08-17 21:41:31 -0300

    replace date string

--- src/utils/fileSystem.js
+++ src/utils/fileSystem.js
@@ -1,12 +1,10 @@
 const generateFileNameByUrl = (url, fileLocation, fileExtension = "") =>
     `${__dirname}${fileLocation}/${new Date()
         .toLocaleTimeString()
-        .replace(
-            /[:|\s]/g,
-            "_"
-        )}${new Date().toLocaleDateString()}${Buffer.from(
-        url,
-        "latin1"
-    ).toString("base64")}${fileExtension}`;
+        .replace(/[:|\s]/g, "_")}${new Date()
+        .toLocaleDateString()
+        .replace(/[/]/g, "")}${Buffer.from(url, "latin1").toString(
+        "base64"
+    )}${fileExtension}`;
 
 module.exports = { generateFileNameByUrl };
